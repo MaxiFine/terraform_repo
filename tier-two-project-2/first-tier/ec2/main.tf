ecd9caf53451bf1a2beff2b1c1b7eb9e1b66cc18
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-08 15:39:52 +0000

    fixes for ec2 errors

--- tier-two-project-2/first-tier/ec2/main.tf
+++ tier-two-project-2/first-tier/ec2/main.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "web1" {
   instance_type               = "t2.micro"
   key_name                    = "tier-2-key"
   availability_zone           = "us-east-1a"
-  vpc_security_group_ids      = [aws_security_group.public_sg.id]
-  subnet_id                   = aws_subnet.public_1.id
+  vpc_security_group_ids      = var.security_group_id
+  subnet_id                   = module.vpc.privatesub_1
   associate_public_ip_address = true
   user_data                   = <<-EOF
         #!/bin/bash
@@ -25,8 +25,11 @@ resource "aws_instance" "web2" {
   instance_type               = var.instance_type
 key_name                    = "tier-2-key"
   availability_zone           = "us-east-1b"
-  vpc_security_group_ids      = [aws_security_group.public_sg.id]
-  subnet_id                   = aws_subnet.public_2.id
+  
+#   vpc_security_group_ids      = [aws_security_group.public_sg.id]
+  vpc_security_group_ids      = var.security_group_id
+  security_groups = module.security_group.security_group_id
+  subnet_id                   = module.vpc.publicsub_2
   associate_public_ip_address = true
   user_data                   = <<-EOF
         #!/bin/bash
@@ -36,6 +39,7 @@ key_name                    = "tier-2-key"
         systemctl enable httpd
         echo "<html><body><h1>Hey fellow Terraform learners!</h1></body></html>" > /var/www/html/index.html
         EOF
+        
 
   tags = {
     Name = "web2_instance"
