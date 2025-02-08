84d80f729102a90b6388a06f7dff6f66593804c1
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-08 01:53:25 +0000

    clean architecture fix

--- tier-two-project-2/first-tier/outputs.tf
+++ tier-two-project-2/first-tier/outputs.tf
@@ -9,64 +9,64 @@ terraform {
 
 # Configs for provider
 provider "aws" {
-    region = "eu-west-1"
-  
+  region = "eu-west-1"
+
 }
 
 # Create the VPC
 resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16"
-    instance_tenancy = "default"
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-    tags = {
-      name = "vpc"
-    }
+  tags = {
+    name = "vpc"
+  }
 }
 
 # Public Subnets
 resource "aws_subnet" "public_subnet-1" {
-    vpc_id =  aws_vpc.vpc.id
-    cidr_block = "10.0.0.0/24"
-    availability_zone = "eu-west-1a"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
 
-    tags = {
-      name = "public_subnet-1"
-    }
+  tags = {
+    name = "public_subnet-1"
+  }
 }
 
 resource "aws_subnet" "public_subnet-2" {
-    vpc_id = aws_vpc.vpc.id
-    cidr_block = "10.0.2.0/24"
-    availability_zone = "eu-west-1b"
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-1b"
 
-    tags = {
-      name = "public_subnet-2" 
-    }
+  tags = {
+    name = "public_subnet-2"
+  }
 }
 
 # Create the private Subnets
 resource "aws_subnet" "private_subnet-1" {
-    vpc_id = aws_vpc.vpc.id
-    cidr_block = "10.0.3.0/24"
-    availability_zone = "eu-west-1a"
-    map_customer_owned_ip_on_launch = false
+  vpc_id                          = aws_vpc.vpc.id
+  cidr_block                      = "10.0.3.0/24"
+  availability_zone               = "eu-west-1a"
+  map_customer_owned_ip_on_launch = false
 
-    tags = {
-      name = "private_subnet"
-    }
+  tags = {
+    name = "private_subnet"
+  }
 }
 
 resource "aws_subnet" "private_subnet-2" {
-  
-    vpc_id = aws_vpc.vpc.id
-    cidr_block = "10.0.4.0/24"
-    availability_zone = "eu-west-1b"
-    map_customer_owned_ip_on_launch = false
 
-    tags = {
-      name = "private_subnet-2"
-    }
+  vpc_id                          = aws_vpc.vpc.id
+  cidr_block                      = "10.0.4.0/24"
+  availability_zone               = "eu-west-1b"
+  map_customer_owned_ip_on_launch = false
+
+  tags = {
+    name = "private_subnet-2"
+  }
 }
 
 
