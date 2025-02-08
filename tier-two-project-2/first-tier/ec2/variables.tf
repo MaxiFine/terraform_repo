92c4560f1d521b2d7a9e1421d1e37f1543e1bc68
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-08 15:40:21 +0000

    fixes for ec2 errors in vars

--- tier-two-project-2/first-tier/ec2/variables.tf
+++ tier-two-project-2/first-tier/ec2/variables.tf
@@ -15,3 +15,26 @@ variable "instance_type" {
     default = "t2.micro"
  
 }
+
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+# Optionally, if you need to reference a public security group:
+variable "public_sg_id" {
+  description = "ID of the public security group"
+  type        = string
+}
+
+variable "security_group_id" {
+    description = "SEcurity group for Ec2"
+    type = list(string)
+}
+
