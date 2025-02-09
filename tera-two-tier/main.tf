28f787b90e45d44dea5a2d6d862edfa4f2fe0924
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-09 22:55:21 +0000

    removing properties errors

--- tera-two-tier/main.tf
+++ tera-two-tier/main.tf
@@ -4,26 +4,26 @@ provider "aws" {
 
 module "vpc" {
   source = "./modules/vpc"
-  vpc_cidr = "10.0.0.0/16"
-  public_subnet_cidr = "10.0.1.0/24"
-  private_subnet_cidr = "10.0.2.0/24"
+#   vpc_cidr = "10.0.0.0/16"
+#   public_subnet_cidr = "10.0.1.0/24"
+#   private_subnet_cidr = "10.0.2.0/24"
 }
 
 module "security_group" {
   source = "./modules/security_group"
-  vpc_id = module.vpc.vpc_id
+#   vpc_id = module.vpc.vpc_id
 }
 
 module "alb" {
   source = "./modules/alb"
-  vpc_id = module.vpc.vpc_id
-  public_subnet_id = module.vpc.public_subnet_id
-  security_group_id = module.security_group.alb_security_group_id
+#   vpc_id = module.vpc.vpc_id
+#   public_subnet_id = module.vpc.public_subnet_id
+#   security_group_id = module.security_group.alb_security_group_id
 }
 
 module "rds" {
   source = "./modules/rds"
-  vpc_id = module.vpc.vpc_id
-  private_subnet_id = module.vpc.private_subnet_id
-  security_group_id = module.security_group.rds_security_group_id
+#   vpc_id = module.vpc.vpc_id
+#   private_subnet_id = module.vpc.private_subnet_id
+#   security_group_id = module.security_group.rds_security_group_id
 }
