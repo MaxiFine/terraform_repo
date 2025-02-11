04e70338128dfba3b1805200bcecea96da0d4eb5
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-11 15:43:08 +0000

    static website hosting configs

--- lab-work-2/main.tf
+++ lab-work-2/main.tf
@@ -29,3 +29,38 @@ resource "aws_s3_bucket_public_access_block" "mx-bucket" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+
+# S3 Static Website Hosting Configurations
+
+resource "aws_s3_bucket_website_configuration" "bucket1" {
+  bucket = aws_s3_bucket.mx-bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+resource "aws_s3_bucket_policy" "public_read_access" {
+  bucket = aws_s3_bucket.mx-bucket.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+   "Principal": "*",
+      "Action": [ "s3:GetObject" ],
+      "Resource": [
+        "${aws_s3_bucket.mx-bucket.arn}",
+        "${aws_s3_bucket.mx-bucket.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+
+
