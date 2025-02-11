a04a1eb8d20dc92fd51269452ce57b3dff800e31
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-11 15:41:09 +0000

    s3 configurations

--- lab-work-2/main.tf
+++ lab-work-2/main.tf
@@ -14,3 +14,18 @@ provider "aws" {
   region = var.aws_region
 }
 
+
+#   S3 BUCKET CREATION
+resource "aws_s3_bucket" "mx-bucket" {
+    bucket = "mx-lab-bucket"
+
+}
+
+# S3 BUCKET ACCESS CONFIGURATION
+resource "aws_s3_bucket_public_access_block" "mx-bucket" {
+  bucket = aws_s3_bucket.mx-bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
---
+++ lab-work-2/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "eu-west-1" 
+  
+}
+
+variable "bucket_name" {
+    description = "AWS S3 Bucket"
+    type = string
+    default = "mx-lab-bucket"
+  
+}
