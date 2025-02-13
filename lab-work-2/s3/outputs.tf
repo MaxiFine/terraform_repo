f4717612e957aedf7889d9a63063df985001006e
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-13 11:26:14 +0000

    refactored output attribute

--- lab-work-2/s3/outputs.tf
+++ lab-work-2/s3/outputs.tf
@@ -1,7 +1,7 @@
 output "website_endpoint" {
-    # value = aws_s3_bucket.mx-bucket.bucket_regional_domain_name
-    value = aws_s3_bucket.mx-bucket.website_domain
-  
+    value = aws_s3_bucket.mx-bucket.bucket_regional_domain_name
+    # value = aws_s3_bucket.mx-bucket.website_domain
+    # value = aws_s3_bucket.mx-bucket.s3_bucket_bucket_re_domain_name
 }
 
 # modules call type
