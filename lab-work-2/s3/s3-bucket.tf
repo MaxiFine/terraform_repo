b269685887fa63ed6845d9373225465ce0163363
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-13 10:49:36 +0000

    optimizing s3 objects codes

--- lab-work-2/s3/s3-bucket.tf
+++ lab-work-2/s3/s3-bucket.tf
@@ -14,29 +14,63 @@ resource "aws_s3_bucket_public_access_block" "mx-bucket" {
   restrict_public_buckets = false
 }
 
-# Puting Object into s3
-resource "aws_s3_bucket_object" "index" {
-  bucket = aws_s3_bucket.mx-bucket.id
-  key    = "index.html"
-#   source = "C:/Users/MaxwellAdomako/terraforms/lab-work-2/error.html"
+# Puting Object into s3 REDUNDANT CODES
+# resource "aws_s3_bucket_object" "index" {
+#   bucket = aws_s3_bucket.mx-bucket.id
+#   key    = "index.html"
+# #   source = "C:/Users/MaxwellAdomako/terraforms/lab-work-2/error.html"
 
-  source =  "${path.root}/index.html" 
-  content_type = "text/html"
+#   source =  "${path.root}/index.html" 
+#   content_type = "text/html"
+# }
+
+# resource "aws_s3_bucket_object" "error" {
+#   bucket = aws_s3_bucket.mx-bucket.id
+#   key    = "error.html"
+#   source = "${path.root}/error.html" 
+#   content_type = "text/html"
+# }
+
+# resource "aws_s3_bucket_object" "about" {
+#   bucket = aws_s3_bucket.mx-bucket.id
+#   key    = "about.html"
+#   source = "${path.root}/about.html" 
+#   content_type = "text/html"
+# }
+
+# USING OPTIMIZED CODES
+# local vars to store the objects
+locals {
+  
+  s3_objects = {
+    index = {
+      key = "index.html"
+      source = "${path.root}/index.html"
+      content_type = "text/html"
+    }
+    error = {
+      key = "error.html"
+      source = "${path.root}/error.html"
+      content_type = "text/html"
+    }
+    about = {
+      key = "about.html"
+      source = "${path.root}/about.html"
+      content_type = "text/html"
+    }
+  }
 }
 
-resource "aws_s3_bucket_object" "error" {
+
+# s3 Objects created using for_each 
+resource "aws_s3_bucket_object" "s3_objects" {
+  for_each = local.s3_objects
   bucket = aws_s3_bucket.mx-bucket.id
-  key    = "error.html"
-  source = "${path.root}/error.html" 
-  content_type = "text/html"
+  key    = each.value.key
+  source = each.value.source
+  content_type = each.value.content_type
 }
 
-resource "aws_s3_bucket_object" "about" {
-  bucket = aws_s3_bucket.mx-bucket.id
-  key    = "about.html"
-  source = "${path.root}/about.html" 
-  content_type = "text/html"
-}
 
 # S3 Static Website Hosting Configurations
 resource "aws_s3_bucket_website_configuration" "mx-bucket" {
