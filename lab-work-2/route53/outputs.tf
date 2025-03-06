863317c3d75b3109ca4f8d9a96fa495e5590ec6f
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-03-06 09:04:23 +0000

    route 53 configs

---
+++ lab-work-2/route53/outputs.tf
@@ -0,0 +1,15 @@
+output "zone_id" {
+  value = aws_route53_zone.main.id
+}
+
+
+output "zone_arn" {
+  value = aws_route53_zone.main.arn
+}
+
+
+output "zone_name" {
+  value = aws_route53_zone.main.name
+}
+
+
---
+++ lab-work-2/route53/variables.tf
@@ -0,0 +1,43 @@
+# variable "domain_name" {
+# description = "Domain name registered for Application"
+# }
+
+# variable "comment" {
+# description = "Comment added in hosted zone"      
+# }
+
+# variable "ttl" {
+# description = "TTL of Record"
+# }
+
+# variable "primaryhealthcheck" {
+# description = "Tag Name for Primary Instance Health Check"
+# }
+
+# variable "secondaryhealthcheck" {
+# description = "Tag Name for Secondary Instance Health Check"
+# }
+
+# variable "subdomain" {
+# description = "Sub Domain name to access Application"
+# }
+
+# variable "identifier1" {
+# }
+
+# variable "identifier2" {
+# }
+
+# variable "primaryip" {
+# }
+
+# variable "secondaryip" {
+# }
+
+
+######################################
+## VARS FOR 
+variable "external_alb_dns" {
+  type = string
+  description = "External ALB as entry  point"
+}
