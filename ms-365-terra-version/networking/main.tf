afbc1f60a82f92d94770d7c274aa16fdc1d765f1
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-06-18 16:08:24 +0000

    add private route table and association for private subnet

--- ms-365-terra-version/networking/main.tf
+++ ms-365-terra-version/networking/main.tf
@@ -62,11 +62,20 @@ resource "aws_route_table" "public_route_table" {
 
 resource "aws_route_table_association" "public_route_table_association" {
     subnet_id      = aws_subnet.public_subnet.id
-    route_table_id = aws_route_table.public_route_table.id
+    route_table_id = aws_route_table.public_route_table.id  
+}
+
+resource "aws_route_table" "private_route_table" {
+    vpc_id = aws_vpc.mx-vpc.id
 
     tags = {
-      Name = "Public Route Table Association"
+      Name = "Private Route Table"
       Environment = "Development"
     }
   
+}
+
+resource "aws_route_table_association" "private_route_table_association" {
+    subnet_id      = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.private_route_table.id
 }
