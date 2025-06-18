c79a924797a667333fa449fc090c843a6cf9eb36
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-06-18 17:04:16 +0000

    fix: remove unused NAT Gateway Elastic IP output from outputs.tf

--- ms-365-terra-version/outputs.tf
+++ ms-365-terra-version/outputs.tf
@@ -33,9 +33,3 @@ output "nat_gateway_id" {
   value       = module.networking.nat_gateway_id
   
 }
-
-output "nat_ip" {
-  description = "The NAT Gateway Elastic IP"
-  value       = module.networking.nat_ip
-  
-}
