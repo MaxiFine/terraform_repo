90d1909575f43794b04bfda0fc06f285ccd6d662
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-06-19 12:22:48 +0000

    feat: add user data script for Nginx installation and configuration

--- ms-365-terra-version/compute/variables.tf
+++ ms-365-terra-version/compute/variables.tf
@@ -24,3 +24,21 @@ variable "aws_region" {
   type        = string
   default     = "eu-north-1"
 }
+
+variable "private_security_group_id" {
+  description = "The ID of the private security group"
+  type        = string
+  
+}
+
+variable "public_subnet_id" {
+  description = "The ID of the public subnet"
+  type        = string
+  
+}
+
+variable "private_subnet_id" {
+  description = "The ID of the private subnet"
+  type        = string
+  
+}
