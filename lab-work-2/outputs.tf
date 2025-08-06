0aca5d9b38a14dbe3c7c4200873b9ed19da6b1b6
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-08-06 05:39:08 +0000

    fixing deploy action error

--- lab-work-2/outputs.tf
+++ lab-work-2/outputs.tf
@@ -3,10 +3,10 @@
 #     value       = aws_s3_bucket.mx-bucket.name
   
 # }
-output "s3_bucket_name" {
-    description = "Name of the S3 bucket"
-    value       = module.s3.name_of_s3_bucket
-}
+# output "s3_bucket_name" {
+#     description = "Name of the S3 bucket"
+#     value       = module.s3.name_of_s3_bucket
+# }
 
 output "s3_bucket_id" {
     description = "ARN of the S3 bucket"
