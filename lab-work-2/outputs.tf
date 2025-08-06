6c91665e92816046562caeea3322a3d128df0cdb
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-08-06 03:21:48 +0000

    fix: output export

--- lab-work-2/outputs.tf
+++ lab-work-2/outputs.tf
@@ -5,15 +5,15 @@
 # }
 output "s3_bucket_name" {
     description = "Name of the S3 bucket"
-    value       = module.s3.bucket_name
-}
-
-output "s3_bucket_arn" {
-    description = "ARN of the S3 bucket"
-    value       = module.s3.bucket_arn
+    value       = module.s3.name_of_s3_bucket
 }
 
 output "s3_bucket_id" {
-    description = "ID of the S3 bucket"
-    value       = module.s3.bucket_id
+    description = "ARN of the S3 bucket"
+    value       = module.s3.s3_bucket_id
 }
+
+# output "s3_bucket_id" {
+#     description = "ID of the S3 bucket"
+#     value       = module.s3.bucket_id
+# }
