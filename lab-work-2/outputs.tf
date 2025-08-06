290178eac3915547f799cf8ff8f517650e5e5cc2
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-08-06 09:51:44 +0000

    fix: Terraform destroy fixing

--- lab-work-2/outputs.tf
+++ lab-work-2/outputs.tf
@@ -1,8 +1,10 @@
-# output "s3_bucket_name" {
-#     description = "Name of the S3 bucket"
-#     value       = aws_s3_bucket.mx-bucket.name
+output "s3_bucket_endpoint" {
+    description = "Name of the S3 bucket"
+    # value       = aws_s3_bucket.modules.s3.website_endpoint
+    value = module.s3.website_endpoint
   
-# }
+}
+
 # output "s3_bucket_name" {
 #     description = "Name of the S3 bucket"
 #     value       = module.s3.name_of_s3_bucket
