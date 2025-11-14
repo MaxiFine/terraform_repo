724fc6ef61b3e24bc87d60a3a1ea3e7886642379
Maxwell Adomako <gibboel5@gmail.com>
2025-11-14 11:05:31 +0000

    fix: update AWS provider version to 6.0 and change default region to eu-west-1

--- multiple_environments/separated_directories/environments/staging/provider.tf
+++ multiple_environments/separated_directories/environments/staging/provider.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 6.0"
     }
   }
  
-  backend "s3" {
-    bucket                  = "tf-awesome-backend"
-    key                     = "multi-environments/staging/terraform.tfstate"
-    region                  = "ap-southeast-1"
-    profile                 = "tf-awesome"
-  }
+  # backend "s3" {
+  #   bucket                  = "tf-awesome-backend"
+  #   key                     = "multi-environments/staging/terraform.tfstate"
+  #   region                  = "ap-southeast-1"
+  #   profile                 = "tf-awesome"
+  # }
 }
 provider "aws" {
-  profile = "tf-awesome"
+  # profile = "tf-awesome"
   region  = var.default_region
 }
 
