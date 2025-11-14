ec5363b53bc5365d861663f5b019b3eec50ad146
Maxwell Adomako <gibboel5@gmail.com>
2025-11-14 10:20:35 +0000

    refactor: update VPC module tags and improve variable descriptions for clarity

--- multiple_environments/workspaces/modules/vpc/variables.tf
+++ multiple_environments/workspaces/modules/vpc/variables.tf
@@ -1,4 +1,6 @@
-variable "env" {}
+variable "env" {
+    description = "Environment For this resources"
+}
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "az_count" { default = 2 }
 variable "enable_nat" { default = true }
