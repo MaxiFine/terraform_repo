10fe50d2dc8ef4a7d44078183de8db690a5d142e
Maxwell Adomako <gibboel5@gmail.com>
2025-12-23 15:12:28 +0000

    feat: update output values for private instance IPs and adjust network interface references in routes

--- self-managed-nat-gateway/modules/compute/outputs.tf
+++ self-managed-nat-gateway/modules/compute/outputs.tf
@@ -1,10 +1,10 @@
 output "private_instance_ip_1" {
   description = "Private instance ip 1 address"
-  value = aws_instance.nat_aws_instances.private_ip
+  value = aws_instance.aws_nat_gateway_instance[*].private_ip
 }
 
 output "private_instance_ip_2" {
   description = "Private instance ip 2 address"
-  value = aws_instance.test_instance.private_ip
+  value = aws_instance.nat_testing_aws_instances[*].private_ip
 }
 
