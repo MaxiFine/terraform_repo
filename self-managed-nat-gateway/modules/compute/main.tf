784189106c6ce4eab1579195407e8a8a3706a8b1
Maxwell Adomako <gibboel5@gmail.com>
2025-12-23 14:28:23 +0000

    feat: add NAT instance and testing instance configurations with security groups and routes

---
+++ self-managed-nat-gateway/modules/compute/main.tf
@@ -0,0 +1,89 @@
+data "aws_ami" "validate" {
+  count  = var.ami_id != null ? 1 : 0
+  owners = ["self", "amazon", "099720109477"]
+
+  filter {
+    name   = "image-id"
+    values = [var.ami_id]
+  }
+}
+
+# Default to latest Ubuntu
+data "aws_ami" "ubuntu_latest" {
+  count       = var.ami_id == null ? 1 : 0
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+locals {
+  selected_ami = var.ami_id != null ? data.aws_ami.validate[0].id : data.aws_ami.ubuntu_latest[0].id
+}
+
+
+#################################
+## USING Page configurations
+data "aws_ami" "amzn_linux_2023_ami" {
+  most_recent                       = true
+  owners                            = ["amazon"]
+  filter {
+    name                            = "name"
+    values                          = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_instance" "nat_aws_instances" {
+    count                           = length(var.public_subnet_ids)
+    ami                             = var.nat_instance_ami_id
+    instance_type                   = "t2.micro"
+    subnet_id                       = element(var.public_subnet_ids[*], count.index)
+    vpc_security_group_ids          = [aws_security_group.nat_instance_sg.id]
+    associate_public_ip_address     = true
+    source_dest_check               = false
+    user_data                       = <<-EOL
+                                        #! /bin/bash
+                                        sudo yum install iptables-services -y
+                                        sudo systemctl enable iptables
+                                        sudo systemctl start iptables
+                                        sudo sysctl -w net.ipv4.ip_forward=1
+                                        sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+                                        sudo /sbin/iptables -F FORWARD
+    EOL
+    user_data_replace_on_change     = true
+    key_name                        = var.ec2_key_name
+
+    root_block_device {
+        volume_size                 = "8"
+        volume_type                 = "gp2"
+        encrypted                   = true
+    }
+    tags                            = "${merge(var.tags,
+                                                tomap({"Name" = "NAT EC2 Instance ${count.index + 1}"}))}"
+}
+
+resource "aws_instance" "nat_testing_aws_instances" {
+    count                           = var.create_nat_testing_instances ? length(var.private_subnet_ids) : 0
+    ami                             = data.aws_ami.amzn_linux_2023_ami.id
+    instance_type                   = "t2.micro"
+    subnet_id                       = element(var.private_subnet_ids[*], count.index)
+    vpc_security_group_ids          = [aws_security_group.nat_testing_instance_sg.id]
+    key_name                        = var.ec2_key_name
+
+    root_block_device {
+        volume_size                 = "8"
+        volume_type                 = "gp2"
+        encrypted                   = true
+    }
+
+    tags                            = "${merge(var.tags,
+                                                tomap({"Name" = "NAT Testing EC2 Instance ${count.index + 1}"}))}"            
+}
---
+++ self-managed-nat-gateway/modules/compute/variables.tf
@@ -0,0 +1,84 @@
+variable "ami_id" {
+  description = "AMI ID to use for EC2 instances."
+  type        = string
+}
+
+variable "tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+  default = {
+    Environment = "Development"
+    ManagedBy   = "Terraform"
+    Project     = "AWS Self Managed NAT Gateway"
+    Owner       = "MxnLabs"
+  }
+}
+
+
+variable "region" {
+  description         = "AWS Region for the service."
+  type                = string
+  default             = "eu-central-1"
+}
+
+variable "nat_instance_ami_id" {
+  description         = "AMI ID for NAT Instance."
+  type                = string
+}
+
+variable "main_vpc_id" {
+  description         = "ID For the VPC ID."
+  type                = string
+}
+
+variable "main_cidr_block" {
+  description         = "CIDR Block for Main VPC."
+  type                = string
+}
+
+variable "public_cidr_blocks" {
+  description        = "Public Subnet CIDR Blocks."
+  type               = list(string)
+}
+ 
+variable "private_cidr_blocks" {
+  description        = "Private Subnet CIDR Blocks"
+  type               = list(string)
+}
+
+variable "public_subnet_ids" {
+  description         = "IDs for the Public Subnets."
+  type                = list(string)
+}
+
+variable "create_nat_testing_instances" {
+  description         = "Flag to add NAT Testing Instances in Private Subnets."
+  type                = bool
+}
+
+variable "private_subnet_ids" {
+  description         = "IDs for the Private Subnets."
+  type                = list(string)
+}
+
+variable "private_subnet_route_table_ids" {
+  description         = "IDs for Route Tables within Private Subnets."
+  type                = list(string)
+}
+
+variable "private_ips_for_ssh" {
+  description         = "IP Addresss which will allow SSH Connections from outside of VPC."
+  type                = list(string)
+}
+
+variable "ec2_key_name" {
+  description         = "Keyname used for ssh access to EC2 instances."
+  type                = string
+}
+
+variable "tags" {
+  description         = "Tags to be added to the EC2 Instances."
+  type                = map(string)
+  default             = {}
+}
+
