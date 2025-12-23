ee80cde47eb2982e2fdba6f19f7def0f0ef44d25
Maxwell Adomako <gibboel5@gmail.com>
2025-12-23 14:27:52 +0000

    feat: add keypair module with TLS private key and AWS key pair outputs

---
+++ self-managed-nat-gateway/modules/keypair/main.tf
@@ -0,0 +1,13 @@
+resource "tls_private_key" "primary_key" {
+    algorithm                       = var.rsa_algorithm
+    rsa_bits                        = var.rsa_bits
+}
+
+
+resource "aws_key_pair" "main" {
+    key_name                        = "${var.key_name}"
+    public_key                      = tls_private_key.primary_key.public_key_openssh
+    tags                            = "${merge(var.tags,
+                                                tomap({"Name" = "${var.key_name} AWS Key Pair"}))}"
+}
+
