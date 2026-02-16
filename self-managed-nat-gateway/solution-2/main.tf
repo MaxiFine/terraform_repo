2ba766a8eb0f6cbe20cdc199fffadd18318f79a2
Maxwell Adomako <gibboel5@gmail.com>
2026-02-16 15:51:13 +0000

    refactor: remove unnecessary output variables for NAT and private test EC2 instances in main.tf

--- self-managed-nat-gateway/solution-2/main.tf
+++ self-managed-nat-gateway/solution-2/main.tf
@@ -396,21 +396,3 @@ resource "aws_instance" "private_test_ec2" {
     }
 }
 
-##########################################
-# Helpful Outputs
-##########################################
-
-output "nat_instance_id" {
-    value       = aws_instance.nat_ec2_instance.id
-    description = "ID of the NAT EC2 instance"
-}
-
-output "nat_instance_eip" {
-    value       = aws_eip.nat_ec2_eip.public_ip
-    description = "Elastic IP associated with NAT EC2"
-}
-
-output "private_test_instance_id" {
-    value       = aws_instance.private_test_ec2.id
-    description = "ID of the private test EC2"
-}
