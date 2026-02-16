abeeacb4d94910563f8d92e3168eb4ecfc5593fe
Maxwell Adomako <gibboel5@gmail.com>
2026-02-16 15:47:08 +0000

    feat: add data sources for AWS region and availability zones in variables.tf

--- self-managed-nat-gateway/solution-2/modules/vpc/variables.tf
+++ self-managed-nat-gateway/solution-2/modules/vpc/variables.tf
@@ -1,8 +1,21 @@
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+locals {
+  az = data.aws_availability_zones.available.names[*] # Select the first available AZ
+}
+
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.11.0.0/16"
 }
+
 
 variable "vpc_name" {
   description = "Custom VPC name"
@@ -11,10 +24,10 @@ variable "vpc_name" {
 }
 
 
-
 variable "private_subnet_count" {
   default = 2
 }
+
 
 variable "public_subnet_count" {
   default = 2
