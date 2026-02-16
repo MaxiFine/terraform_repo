672b064384c2cea96729079ac99e454f8969188a
Maxwell Adomako <gibboel5@gmail.com>
2026-02-16 15:46:58 +0000

    refactor: remove hardcoded subnet definitions and use local variables for public and private subnets in main.tf

--- self-managed-nat-gateway/solution-2/modules/vpc/main.tf
+++ self-managed-nat-gateway/solution-2/modules/vpc/main.tf
@@ -1,13 +1,3 @@
-data "aws_region" "current" {}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-locals {
-  az = data.aws_availability_zones.available.names[*] # Select the first available AZ
-}
-
 module "vpc" {
     source  = "terraform-aws-modules/vpc/aws"
     version = "~> 6.0"
@@ -17,8 +7,8 @@ module "vpc" {
 
     # Define AZs and subnet ranges
     azs             = [local.az[0], local.az[1]] # 2 Availability Zones
-    private_subnets = ["10.11.1.0/24", "10.11.2.0/24"]    # 256 IPs each
-    public_subnets  = ["10.11.101.0/24", "10.11.102.0/24"] # 256 IPs each
+    public_subnets  = local.public_subnets  
+    private_subnets = local.private_subnets
 
     enable_nat_gateway = false # Using custom NAT instance instead of managed NAT gateway
 
